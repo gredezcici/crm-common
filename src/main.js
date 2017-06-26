ce8b5eab93745be2852a7987e4dd3ed9c492e433
gredezcici <gredezcici@163.com>
2017-06-27 01:42:37 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+
+import routes from './router'
+import VueRouter from 'vue-router'
+import AppView from './App.vue'
+import store from './store'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import '../node_modules/admin-lte/dist/css/AdminLTE.min.css'
+import '../node_modules/admin-lte/dist/css/skins/skin-blue.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min'
+import '../node_modules/admin-lte/dist/js/app.min'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+// Import Helpers for filters
+import { domain, count, prettyDate, pluralize } from './filters/filters'
+
+// Import Install and register helper items
+Vue.filter('count', count)
+Vue.filter('domain', domain)
+Vue.filter('prettyDate', prettyDate)
+Vue.filter('pluralize', pluralize)
+// Routing logic
+var router = new VueRouter({
+  routes: routes
+
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#root',
+  router: router,
+  store: store,
+  render: h => h(AppView)
+})
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import TopBar from '.././components/TopBar.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: TopBar,
+    name: 'nav'
+  }
+]
+
+export default routes

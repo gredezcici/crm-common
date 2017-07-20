c16c93b50a6457a22a45781c85b9acdcf718f1a4
gredezcici <gredezcici@163.com>
2017-07-21 01:10:34 +0800

    changes: icon item list

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import TopBar from '.././components/TopBar.vue'
+import LoginView from '.././components/Login.vue'
 
 const routes = [
+  {
+    path: '/login',
+    component: LoginView
+  },
   {
     path: '/',
     component: TopBar,

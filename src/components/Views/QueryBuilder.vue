83c18d20b7904ab4cd7ca9361ec55da37098eb90
gredezcici <gredezcici@163.com>
2017-08-05 21:45:32 +0800

    changes: query builder daily update

--- src/components/Views/QueryBuilder.vue
+++ src/components/Views/QueryBuilder.vue
@@ -66,23 +66,13 @@
         <!-- Custom Tabs (Pulled to the right) -->
         <div class="nav-tabs-custom">
           <ul class="nav nav-tabs">
-            <li class="active"><a href="#tab_1-1" data-toggle="tab">Tab 1</a></li>
+            <li class="active"><a href="#tab_1-1" data-toggle="tab">Query result</a></li>
             <li><a href="#tab_2-2" data-toggle="tab">Tab 2</a></li>
             <li><a href="#tab_3-2" data-toggle="tab">Tab 3</a></li>
           </ul>
           <div class="tab-content">
             <div class="tab-pane active" id="tab_1-1">
-              <b>How to use:</b>
-
-              <p>Exactly like the original bootstrap tabs except you should use
-                the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
-              A wonderful serenity has taken possession of my entire soul,
-              like these sweet mornings of spring which I enjoy with my whole heart.
-              I am alone, and feel the charm of existence in this spot,
-              which was created for the bliss of souls like mine. I am so happy,
-              my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
-              that I neglect my talents. I should be incapable of drawing a single stroke
-              at the present moment; and yet I feel that I never was a greater artist than now.
+              
             </div>
             <!-- /.tab-pane -->
             <div class="tab-pane" id="tab_2-2">

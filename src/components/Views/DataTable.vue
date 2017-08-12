76cbd3b49a4a2bd655d3ab488c588da80dd86b70
gredezcici <gredezcici@163.com>
2017-08-12 20:02:44 +0800

    changes: query builder daily update

--- src/components/Views/DataTable.vue
+++ src/components/Views/DataTable.vue
@@ -3,7 +3,6 @@
 </template>
 <script>
   import Vue from 'vue'
-
   var dataSet = [
     ['Tiger Nixon', 'System Architect', 'Edinburgh', '5421', '2011/04/25', '$320,800'],
     ['Garrett Winters', 'Accountant', 'Tokyo', '8422', '2011/07/25', '$170,750'],

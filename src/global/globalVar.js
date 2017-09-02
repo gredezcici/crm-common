9fe26124d81ad6439151796328bfd9576f66046f
gredezcici <gredezcici@163.com>
2017-09-02 20:01:32 +0800

    changes: query builder daily update

--- src/global/globalVar.js
+++ src/global/globalVar.js
@@ -7,12 +7,16 @@ Vue.mixin({
     getMyQueryList (uid) {
       console.log(uid)
       let temp = {
-        'count': 4,
+        'count': 8,
         'rows': [
           {'name': 'key customers_new', 'id': '001'},
           {'name': 'potential customers', 'id': '002'},
           {'name': 'overseas', 'id': '003'},
-          {'name': 'local customer', 'id': '004'}
+          {'name': 'local customer', 'id': '004'},
+          {'name': 'key customers_new', 'id': '005'},
+          {'name': 'potential customers_1', 'id': '006'},
+          {'name': 'overseas_1', 'id': '007'},
+          {'name': 'local customer_1', 'id': '008'}
         ]
       }
       this.$store.commit('SET_QUERY_LIST', temp)

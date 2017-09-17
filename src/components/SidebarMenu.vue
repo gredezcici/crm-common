bfc7eecc28286b18e4fa07244299998f06459eb8
gredezcici <gredezcici@163.com>
2017-09-17 22:02:31 +0800

    changes: query builder daily update

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -29,37 +29,6 @@
           <span class="page">{{item.name}}</span>
         </router-link>
       </li>
-
-      <!--<li class="header">LOGS</li>-->
-      <!--<li class="pageLink" v-on:click="toggleMenu">-->
-      <!--<router-link to="/access"><i class="fa fa-book"></i>-->
-      <!--<span class="page">Access</span>-->
-      <!--</router-link>-->
-      <!--</li>-->
-      <!--<li class="pageLink" v-on:click="toggleMenu">-->
-      <!--<router-link to="/server"><i class="fa fa-hdd-o"></i>-->
-      <!--<span class="page">Server</span>-->
-      <!--</router-link>-->
-      <!--</li>-->
-      <!--<li class="pageLink" v-on:click="toggleMenu">-->
-      <!--<router-link to="/repos"><i class="fa fa-heart"></i>-->
-      <!--<span class="page">Repos</span>-->
-      <!--<small class="label pull-right bg-green">AJAX</small>-->
-      <!--</router-link>-->
-      <!--</li>-->
-
-      <!--<li class="header">PAGES</li>-->
-      <!--<li class="pageLink" v-on:click="toggleMenu">-->
-      <!--<router-link to="/login">-->
-      <!--<i class="fa fa-circle-o text-yellow"></i>-->
-      <!--<span class="page"> Login</span>-->
-      <!--</router-link>-->
-      <!--</li>-->
-      <!--<li class="pageLink" v-on:click="toggleMenu">-->
-      <!--<router-link to="/404"><i class="fa fa-circle-o text-red"></i>-->
-      <!--<span class="page"> 404</span>-->
-      <!--</router-link>-->
-      <!--</li>-->
     </ul>
   </div>
 </template>
